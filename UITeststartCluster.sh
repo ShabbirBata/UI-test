0fe869e42e01ed1fb574e5c62f12ec838ed96fad
Shabbir Bata's Dell <sbata@pieriandx.com>
2018-08-20 14:04:45 -0500

    Script update for ECS

--- UITeststartCluster.sh
+++ UITeststartCluster.sh
@@ -4,5 +4,5 @@ cat $2/$1_StackTemplate.json | jq '.Stacks[].Parameters | map(if .ParameterKey =
 aws cloudformation update-stack --stack-name $1 --use-previous-template --parameters file://$2/$1_StackParameters.json --profile <profile name>
 
 #Send notification in Slack channel for this project.
-json="{\"channel\": \"ui-testing\", \"username\":\"Cluster-status-notification\", \"icon_emoji\":\"slack\", \"attachments\":[{\"color\":\"#0000FF\" , \"text\": \"$1 cluster is Started. \"}]}"
+json="{\"channel\": \"slack-channel-name-here\", \"username\":\"Cluster-status-notification\", \"icon_emoji\":\"slack\", \"attachments\":[{\"color\":\"#0000FF\" , \"text\": \"$1 cluster is Started. \"}]}"
 curl -s -d "payload=$json" "<Slack token here>"
